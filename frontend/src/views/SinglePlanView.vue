1e2ef8b5bbeaf1dadb3818fb1ce7f37456112458
Julian Garcia <jgarcg.uc.cl>
2025-12-08 19:07:17 -0300

    added no reviews message

--- frontend/src/views/SinglePlanView.vue
+++ frontend/src/views/SinglePlanView.vue
@@ -9,13 +9,13 @@ import { useAuthStore } from '@/stores/useAuthStore';
     const route = useRoute()
     const auth = useAuthStore()
 
-    // leer un parámetro
     const id = computed(() =>Number(route.params.id)); 
 
     const {plan} = usePlan(id);
-    const {ratings, rating, addRating} = usePlanRatings(id) // ojala unir esto con el de arriba
-</script>
+    const {ratings, rating, addRating} = usePlanRatings(id)
 
+    const other_peoples_ratings = computed(() => ratings.value.filter((r) => r.user_id != auth.user?.id))
+</script>
 
 <template>
     <div class="text-xl">{{ plan?.activity }}</div>
@@ -32,12 +32,16 @@ import { useAuthStore } from '@/stores/useAuthStore';
     <div class="px-4 py-4 border-4 border-gray-400 rounded-md bg-gray-200 inline-block">
         <ChungungoRating :rating="rating?.rating" :changeRating="addRating" />
     </div>
+    <div class="mt-4">Otras calificaciones</div>
     <div>
-        <div v-for="other_rating in ratings.filter((r) => r.user_id != auth.user?.id)">
+        <div v-for="other_rating in other_peoples_ratings">
             <div>
                 {{ other_rating.user.name }}
             </div>
             <ChungungoRating :disabled="true" :rating="other_rating.rating"/>
+        </div>
+        <div v-if="!other_peoples_ratings.length">
+            No hay más calificaciones
         </div>
     </div>
     
